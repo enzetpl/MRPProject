df65cbc083132c7658a27dedb40cb2181e9d490f
Sebastian Staszczyk <sebastian.staszczyk@finanteq.com>
2022-03-14 18:37:52 +0100

    Initial Commit

---
+++ Shared/MRPProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRPProjectApp.swift
+//  Shared
+//
+//  Created by sebastianstaszczyk on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MRPProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
